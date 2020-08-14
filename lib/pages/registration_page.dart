2704768792862a235070099a78a4b74fdd110500
Probe-House <bheer31@gmail.com>
2020-08-13 19:44:03 -0500

    unique user

--- lib/pages/registration_page.dart
+++ lib/pages/registration_page.dart
@@ -4,6 +4,9 @@ import 'package:flutter_flash_chat/pages/chat_page.dart';
 import 'package:flutter_flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final databaseReference = Firestore.instance;
 
 class RegistrationPage extends StatefulWidget {
   static const String id = 'registration_page';
@@ -16,6 +19,44 @@ class _RegistrationPageState extends State<RegistrationPage> {
   bool showSpinner = false;
   String email;
   String password;
+
+  void createRecord(String email) async {
+    final snapShot =
+        await Firestore.instance.collection('users').document(email).get();
+
+    if (snapShot == null || !snapShot.exists) {
+      await databaseReference
+          .collection("users")
+          .document(email)
+          .setData({'email': email});
+    } else {
+      print("user exists!");
+    }
+  }
+
+  void _showDialog() {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Error !"),
+          content: new Text("El email ingresado ya existe."),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,14 +115,36 @@ class _RegistrationPageState extends State<RegistrationPage> {
                     showSpinner = true;
                   });
                   try {
-                    final newUser = await _auth.createUserWithEmailAndPassword(
-                        email: email, password: password);
-                    if (newUser != null) {
-                      Navigator.pushNamed(context, ChatPage.id);
+                    final snapShot = await Firestore.instance
+                        .collection('users')
+                        .document(email)
+                        .get();
+
+                    if (snapShot == null || !snapShot.exists) {
+                      await databaseReference
+                          .collection("users")
+                          .document(email)
+                          .setData({'email': email});
+
+                      final newUser =
+                          await _auth.createUserWithEmailAndPassword(
+                              email: email, password: password);
+                      if (newUser != null) {
+                        print("user " + newUser.user.toString());
+
+                        createRecord(newUser.user.email);
+                        Navigator.pushNamed(context, ChatPage.id);
+                      }
+                      setState(() {
+                        showSpinner = false;
+                      });
+                    } else {
+                      print("user exists!");
+                      _showDialog();
+                      setState(() {
+                        showSpinner = false;
+                      });
                     }
-                    setState(() {
-                      showSpinner = false;
-                    });
                   } catch (e) {
                     print(e);
                   }
